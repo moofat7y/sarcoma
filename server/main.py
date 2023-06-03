f8a9ecc35a23484e42926ff2e7527a41a6d6a6f3
moofat7y <mohammedbelal802@gmail.com>
2023-06-04 00:30:45 +0300

    sec commit

---
+++ server/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI,File,UploadFile,Request;
+from fastapi.middleware.cors import CORSMiddleware
+from tensorflow.keras import models
+import uvicorn
+from utils.utilFun import read_file_as_image
+app = FastAPI()
+MODEL = models.load_model(r'D:\Projects\Full Stack\sarcoma\server\saved_models\model1.h5')
+CLASS_NAME = ['Not effected','Effected']
+
+origins = ["http://localhost:5173","http://localhost:5174"]
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=['*'],allow_headers=['*'])
+
+
+@app.post('/predict')
+async def predict(file : UploadFile = File(...)):
+    image = read_file_as_image(await file.read())
+    predictions = MODEL.predict(image)[0][0]
+    label = round(predictions)
+    confidence = 0
+    if predictions >= 0.5:
+        confidence = predictions
+    else:
+        confidence = 1 - predictions 
+    return {
+        'Diagnose' : CLASS_NAME[label],
+        'Confidence' :int(round(confidence,2) * 100)
+    }
+if __name__ == "__main__":
+    uvicorn.run(app,host='localhost',port=8000)
---
+++ server/utils/utilFun.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import cv2
+from keras.applications.nasnet import preprocess_input
+
+def read_file_as_image(data) -> np.ndarray:
+    img = np.array(Image.open(BytesIO(data)))
+
+    # img = cv2.imread(image)
+    img = cv2.resize(img, (96, 96))
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+sys.modules[read_file_as_image]= {read_file_as_image}
