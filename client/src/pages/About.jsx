b0b87bea1c8529365a5d20750c91aee195db45a9
moofat7y <mohammedbelal802@gmail.com>
2023-06-05 23:42:53 +0300

    third commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,249 @@
 import React from "react";
-
+import image1 from "../assets/image1.jpg";
 const About = () => {
-  return <div className="pt-5"></div>;
+  return (
+    <div style={{ paddingTop: "90px" }} className="">
+      <section className=" pb-2">
+        <h3 className="text-center mb-5">
+          What is soft tissue <span className="text-secondary">sarcoma</span> ?
+        </h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 order-2 order-md-1 col-md-6">
+              <p>
+                skeletal and motor system, peripheral nerves, blood vessels,
+                fat, or the walls of various organs.
+                <br />
+                The prevalence of sarcomas is generally 1% of all malignant
+                tumors. <br />
+                Soft tissue sarcoma can arise in practically any organ of the
+                body, such as: the limbs, and internal organs such as the
+                stomach, intestines, kidneys, lungs, breast, urinary tract,
+                reproductive system, abdominal wall, chest, and back.
+                <br />
+                <br />
+                The most prominent types of soft tissue sarcoma There are
+                several types of soft tissue sarcoma depending on the soft
+                tissues from which the tumor was formed, most notably:
+                Liposarcoma: originates in adipose tissue.
+                <br />
+                Angiosarcoma: develops from blood vessels.
+                <br />
+                <br />
+                Rhabdomyosarcoma: Sarcoma, which comes from skeletal muscles, is
+                the most common sarcoma in children.
+                <br /> Sarcomas are common in children and adults, and in adults
+                common sarcomas are those that come from fat, nerves, blood
+                vessels, muscle envelope, and connective tissue. <br />
+                The average time between the onset of sarcoma symptoms in a limb
+                until a diagnosis is determined is three to six months. <br />
+                In any case of suspicion of soft tissue sarcoma, a specialist
+                sarcoma oncology team working in a joint unit combining
+                orthopedics and oncology should be contacted.
+                <br />
+                <br /> The staff includes a sarcoma oncologist, an orthopedic
+                surgeon specializing in musculoskeletal tumors, a surgeon
+                specializing in internal tumor resection, a pathologist
+                (Pathologist), a radiologist, an anesthesiologist and pain
+                therapist, a physiotherapist and a nurse.
+              </p>
+            </div>
+            <div className="col-12 order-1 order-md-2 col-md-6 mb-3 mb-md-0">
+              <div className="image ">
+                <img
+                  className="ratio rounded-3 ratio-1x1"
+                  src={image1}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-5 pb-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 ">
+              <h3 className="mb-3">Symptoms of soft tissue sarcoma</h3>
+              At the beginning of the disease, the size of the tumor may not be
+              sufficient to reach the nerves and the appearance of symptoms, but
+              as the disease progresses, symptoms occur.
+              <br /> Symptoms occur after the mass grows at the site of cancer,
+              and the symptoms vary according to the location of the disease as
+              follows: Symptoms of a lump in the abdomen:
+              <br /> This causes abdominal pain, a feeling of fullness, and
+              constipation.
+              <br /> Symptoms of a lump in the lung: This causes
+              coughing,shortness of breath.
+              <br /> Causes and risk factors of soft tissue sarcoma There are
+              many causes and factors for soft tissue sarcoma, which are as
+              follows:
+              <br />
+              <br />
+              <ol>
+                <li>
+                  Causes of soft tissue sarcoma The causes of soft tissue
+                  sarcoma are unclear, but in general, soft tissue sarcoma is
+                  caused by a genetic mutation that causes abnormal cell growth
+                  and accumulation until a tumor form.
+                </li>
+                <br />
+                <li>
+                  Risk factors There are several factors that increase the risk
+                  of soft tissue sarcoma:
+                  <br /> Genetic factor The chances of soft tissue sarcoma
+                  increase when there is a family genetic history of infection
+                  or the presence of familial genetic diseases, such as:
+                  neurofibromatosis, Gardner syndrome, colonpolyps, and chronic
+                  swelling of the limbs.
+                </li>
+              </ol>
+              Exposure to chemicals Exposure to certain chemicals, such as
+              pesticides and arsenic, may increase the risk of soft tissue
+              sarcoma.
+              <br /> Radiation exposure Previous radiation therapy for other
+              cancers can increase the risk of soft tissue sarcoma.
+              <br /> Aging The disease occurs at all ages, but it is more common
+              in middle age and adults, so advancing age is a factor that
+              increases the risk of this type of tumor.
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-5 pb-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 ">
+              <h3 className="mb-3">Complications of soft tissue sarcoma</h3>
+              The most prominent complications of soft tissue sarcoma are:
+              <br />
+              Cancer has spread to other areas of the body. Catching the
+              infection due to weakened immunity that occurs as a complication
+              related to the treatment of soft tissue sarcoma. Diagnosis of soft
+              tissue sarcoma If the doctor suspects the presence of soft tissue
+              sarcoma, the doctor will perform the following tests: <br />
+              <br />
+              <ol>
+                <li>
+                  Physical examination The doctor makes a close look at all the
+                  lumps in the body.
+                </li>
+                <br />
+                <li>
+                  Imaging examinations To assess the area containing sarcoma,
+                  the following tests are performed: Ultrasonography: Abdominal
+                  imaging is often done in this method. MRI: This imaging is
+                  performed on the arms and legs. X-ray imaging: This method may
+                  sometimes be used.
+                </li>
+                <br />
+                <li>
+                  Biopsy In some cases, the doctor may resort to taking a biopsy
+                  of the mass to look at it and examine it under a microscope,
+                  often the sample is taken using a fine needle, but in some
+                  cases the biopsy may require a minor operation.
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-5 pb-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 ">
+              <h3 className="mb-3">Treatment of soft tissue sarcoma</h3>
+              Soft tissue sarcoma is treated depending on the size, type, and
+              location of the tumor, but in general treatment is done using the
+              following methods:
+              <br />
+              <br />
+              <ol>
+                <li>
+                  Perform surgery Surgery is a common way to treat soft tissue
+                  sarcoma, which is used if the disease is detected early, often
+                  through which the tumor and part of the surrounding cells are
+                  removed to ensure that all cancer cells are eliminated.
+                  Efforts are made to minimize the impact of surgery on the
+                  appearance and function of the affected part of the body, but
+                  there is a possibility that the patient will have some
+                  difficulty using the affected part of the body after surgery,
+                  and sometimes another surgery may be needed to repair it. In
+                  cases of sarcoma in the limbs, such as: legs and arms, it is
+                  preferable to resort to radiation therapy, or chemotherapy to
+                  reduce the size of the tumor and avoid the need for
+                  amputation, but in some cases surgery is necessary and
+                  amputation is unfortunately the only appropriate option.
+                </li>
+                <br />
+                <li>
+                  Radiation therapy Radiation therapy is used through the use of
+                  high-energy beams, and it is used in one of the following
+                  ways: Before surgery: Preoperative radiation therapy is used
+                  to shrink the tumor and facilitate its removal. During
+                  surgery: Radiation during surgery allows a higher dose to be
+                  delivered directly to the target area, avoiding access to
+                  surrounding tissue. After surgery: Radiation therapy after
+                  surgery helps kill and get rid of any remaining cancer cells
+                  after the procedure. Treatment without resorting to surgery:
+                  It is used to relieve symptoms, and not to worsen the disease
+                  if surgery is not possible. There are many side effects of
+                  radiation therapy, most notably: dermatitis, fatigue, and hair
+                  loss. The purposes are often temporary and the patient
+                  improves within a few days or weeks of the end of radiation
+                  therapy.
+                </li>
+                <br />
+                <li>
+                  Chemotherapy Chemotherapy is used to kill cancer cells, and
+                  chemotherapy may be oral, or intravenously, often chemotherapy
+                  is used intravenously before the operation to shrink the tumor
+                  and facilitate the possibility of its removal. Chemotherapy is
+                  also used alone, or in combination with radiation therapy if
+                  surgical intervention is not possible. After soft tissue
+                  sarcoma types respond to chemotherapy more than others, and
+                  chemotherapy is often used in rhabdomyosis. Side effects of
+                  chemotherapy include: Feeling of permanent fatigue. Feeling
+                  sick. Hair loss. Symptoms may be tiring but temporary and
+                  don't last forever.
+                </li>
+                <br />
+                <li>
+                  Targeted drug therapy Some types of soft tissue sarcoma have
+                  specific characteristics in their cells that can be attacked
+                  by targeted drug therapies, these drugs work better than
+                  chemotherapy and are almost non-toxic. These targeted
+                  therapies are particularly useful in gastrointestinal stromal
+                  tumors.
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-5 pb-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 ">
+              <h3 className="mb-3">Prevention of soft tissue sarcoma</h3>
+              <p>
+                Most cases are hereditary or due to factors that are difficult
+                to prevent, but there are some factors that can be prevented,
+                such as: Avoid exposure to toxic chemicals, such as pesticides.
+                <br />
+                <br />
+                Be careful to stay away from rays as much as possible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
